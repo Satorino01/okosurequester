f706bbec4495d30cdc5c25228053d1420029eeab
Satorino01 <satorino0821@gmail.com>
2018-05-29 20:45:42 +0900

    リクエスト画面の作成

--- app/src/main/java/com/example/kobayashi_satoru/okosurequester/MainActivity.java
+++ app/src/main/java/com/example/kobayashi_satoru/okosurequester/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.kobayashi_satoru.okosurequester;
 
 
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +12,9 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +24,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+public class MainActivity extends AppCompatActivity implements TextWatcher,View.OnClickListener {
     private String urlTxt;
     //データベースの呼び出し
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference dbSendUrl = database.getReference("url");
+    DatabaseReference dbOkitaSum = database.getReference("okita");
+    //タイマー処理用
+    private Timer mTimer = new Timer();
+    Handler mHandler = new Handler();
+    int okitaSum = 0;
+    int kakosum = 0;
+    boolean first = true;
+
     String TAG = "satoru";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +50,134 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         //editTextのリアルタイム監視に必要なメソッド
         EditText edittext = findViewById(R.id.editText);
         edittext.addTextChangedListener(this);
+        //image画像のセット
+        final ImageView okitaimageview = findViewById(R.id.OkitaImageView);
+
+        //タイマー処理
+        mTimer = new Timer(true);
+        mTimer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+                mHandler.post( new Runnable() {
+                    public void run() {
+                        //データベースに読み込み
+                        ValueEventListener valueEventListener = dbOkitaSum.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                // This method is called once with the initial value and again
+                                // whenever data at this location is updated.
+                                try {
+                                    okitaSum = dataSnapshot.getValue(int.class);
+                                } catch (NullPointerException e) {
+                                    return;
+                                }
+
+                                kakosum=okitaSum;
+                                if(first) {
+                                    first = false;
+                                    Log.d("変数の中身は", okitaSum + "でええすあああああああ");
+                                    if (okitaSum >= 10) {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita4);
+                                        Log.d("画像の中身を４に", "画像を切り替えたあああああああ");
+                                    } else if (okitaSum >= 5) {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita3);
+                                        Log.d("画像の中身を３に", "画像を切り替えたあああああああ");
+                                    } else if (okitaSum >= 1) {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita2);
+                                        Log.d("画像の中身を２に", "画像を切り替えたあああああああ");
+                                    } else {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita1);
+                                        Log.d("画像の中身を１に", "画像を切り替えたあああああああ");
+                                    }
+                                }
+                                if(kakosum==okitaSum){
+                                }
+                                else{
+                                    Log.d("変数の中身は", okitaSum + "でええすあああああああ");
+                                    if (okitaSum >= 10) {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita4);
+                                        Log.d("画像の中身を４に", "画像を切り替えたあああああああ");
+                                    } else if (okitaSum >= 5) {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita3);
+                                        Log.d("画像の中身を３に", "画像を切り替えたあああああああ");
+                                    } else if (okitaSum >= 1) {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita2);
+                                        Log.d("画像の中身を２に", "画像を切り替えたあああああああ");
+                                    } else {
+                                        //イメージのサイズを指定
+                                        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(800, 800);
+                                        //表示位置を指定
+                                        params.topMargin = 160;
+                                        params.leftMargin = 200;
+                                        //イメージの表示
+                                        okitaimageview.setLayoutParams(params);
+                                        okitaimageview.setImageResource(R.drawable.okita1);
+                                        Log.d("画像の中身を１に", "画像を切り替えたあああああああ");
+
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError error) {
+                                // Failed to read value
+                                Log.w(TAG, "Failed to read value.", error.toException());
+                            }
+                        });
+                    }
+                });
+            }
+        },1000,10000); //1秒後から10秒間隔で実行
+
     }
     @Override
     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -46,7 +193,6 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
     public void afterTextChanged(Editable editable) {
         urlTxt=editable.toString();
     }
-
     public void onClickSend(View view) {
         if(urlTxt!=null) {
             //データベースに書き込み
@@ -57,5 +203,22 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
             Toast toast = Toast.makeText(this,"URLを入力してね", Toast.LENGTH_LONG);
             toast.show();
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+    public void onClickTimeInputButton(View v){
+        Toast toast = Toast.makeText(this,"未実装", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public void onClickAlarmSetButton(View v){
+        Toast toast = Toast.makeText(this,"未実装", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public void onClickUrlInputButton(View v){
+        Toast toast = Toast.makeText(this,"未実装", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
